7b4d673ddca06428667624919c70c99c4166ca46
EVGESHAd <root@ih370464.vds.myihor.ru>
2017-08-14 17:46:33 +0300

    Add linux launcher

--- EBPLauncher/main.cpp
+++ EBPLauncher/main.cpp
@@ -1,31 +1,44 @@
-#include <windows.h>
-#include <string>
-#include <iostream>
-#include <cstdlib> // для system
-#include "../EBPCore/ebp_api.h"
-#include <thread>
+#ifdef __linux__
+	#include <string>
+	#include <iostream>
+	#include <cstdlib> // для system
+	#include "ebp_api.h"
+	#include "host.h"
+	#include <thread>
+#elif _WIN32
+	#include <windows.h>
+	#include <string>
+	#include <iostream>
+	#include <cstdlib> // для system
+	#include "../EBPCore/ebp_api.h"
+	#include <thread>
+	
+	typedef inputApi(*pfnInit)(const std::string botname, outputApi api);
+	typedef void(*pfnShutdown)(void);
+
+	pfnInit Host_Main;
+	pfnShutdown Host_Shutdown;
+	HINSTANCE	hEngine;
+#endif
 
 #define BOT_PATH	"bot"	// default dir to start from
 
-typedef inputApi(*pfnInit)(const std::string botname, outputApi api);
-typedef void(*pfnShutdown)(void);
-
-pfnInit Host_Main;
-pfnShutdown Host_Shutdown;
-HINSTANCE	hEngine;
-
 void Sys_LoadEBP(void)
 {
+	#ifdef _WIN32
 	hEngine = LoadLibrary("EBPCore.dll");
 	Host_Main = (pfnInit)GetProcAddress(hEngine, "Host_Main");
 	Host_Shutdown = (pfnShutdown)GetProcAddress(hEngine, "Host_Shutdown");
 	api_output.writeline("{8}EBPCore.dll has been loaded\n");
+	#endif
 }
 
 void Sys_UnloadEngine(void)
 {
 	if (Host_Shutdown) Host_Shutdown();
+	#ifdef _WIN32
 	if (hEngine) FreeLibrary(hEngine);
+	#endif
 }
 
 void instructionLoop();
--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 git clone https://github.com/vinniefalco/LuaBridge
 make -f makefile_core
+make -f makefile_launcher
